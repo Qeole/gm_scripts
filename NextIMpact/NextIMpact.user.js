d3c22f1556ac177a16135bef88acbf39852ff1bf
Qeole <qeole@outlook.com>
2015-01-17 00:54:57 +0100

    Adding NextINpact script

---
+++ NextIMpact/NextIMpact.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        NextINpact
+// @namespace   qeole
+// @description NextINpact.om: Adds in Header
+// @include     http://www.nextinpact.com/*
+// @version     1.0.0
+// @grant       none
+// ==/UserScript==
+let h = document.getElementById("container_ban_pub_jauge");
+h.parentNode.removeChild(h);
